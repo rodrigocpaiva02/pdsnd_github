6ad05b09336f1d14034a1683c9b6cc726677992d
Rodrigo Paiva <rpaiva21@its.jnj.com>
2023-12-27 11:19:16 -0500

    Optimize Station Statistics code

--- bikeshare.py
+++ bikeshare.py
@@ -102,6 +102,8 @@ def time_stats(df):
 def station_stats(df):
     """Displays statistics on the most popular stations and trip."""
 
+    # Code that optimizes station statistics...
+
     print('\nCalculating The Most Popular Stations and Trip...\n')
     start_time = time.time()
 
